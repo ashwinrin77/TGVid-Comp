2521e8918131b0221e472822335d3e61581d4b76
ashwinrin77 <161035206+ashwinrin77@users.noreply.github.com>
2024-06-25 11:04:33 -1200

    Update config.py

--- config.py
+++ config.py
@@ -7,7 +7,7 @@ class Config(object):
     # pyro client config
     API_ID    = os.environ.get("API_ID", "28015531")  # ⚠️ Required
     API_HASH  = os.environ.get("API_HASH", "2ab4ba37fd5d9ebf1353328fc915ad28") # ⚠️ Required
-    BOT_TOKEN = os.environ.get("BOT_TOKEN", "7078174063:AAHh7wcXOvYEaLrEzW_Rc1X0lfarFIvXdMQ") # ⚠️ Required
+    BOT_TOKEN = os.environ.get("BOT_TOKEN", "7353099372:AAHKDUqGrZWuGmGcVbDyxle-WcLAjoP9AWk") # ⚠️ Required
     FORCE_SUB = os.environ.get('FORCE_SUB', '@animetamil_xyz') # ⚠️ Required
     AUTH_CHANNEL = int(FORCE_SUB) if FORCE_SUB and id_pattern.search(
     FORCE_SUB) else None
@@ -18,7 +18,7 @@ class Config(object):
 
     # Other Configs 
     ADMIN = int(os.environ.get("ADMIN", "6121610691")) # ⚠️ Required
-    LOG_CHANNEL = int(os.environ.get('LOG_CHANNEL', '-1002149776741')) # ⚠️ Required
+    LOG_CHANNEL = int(os.environ.get('LOG_CHANNEL', '-1002170801684')) # ⚠️ Required
     BOT_UPTIME = BOT_UPTIME  = time.time()
     START_PIC = os.environ.get("START_PIC", "https://graph.org/file/15e82d7e665eccc8bd9c5.jpg")
 
