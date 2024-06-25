d9d98d19e71571ea6d0f13e30c58b0c73e57973c
ashwinrin77 <161035206+ashwinrin77@users.noreply.github.com>
2024-06-25 09:38:11 -1200

    Update config.py

--- config.py
+++ config.py
@@ -7,7 +7,7 @@ class Config(object):
     # pyro client config
     API_ID    = os.environ.get("API_ID", "28015531")  # ⚠️ Required
     API_HASH  = os.environ.get("API_HASH", "2ab4ba37fd5d9ebf1353328fc915ad28") # ⚠️ Required
-    BOT_TOKEN = os.environ.get("BOT_TOKEN", "7072790150:AAGl-Vjbu0mZJzGHii36Ax-cRR1j5wxZW7M") # ⚠️ Required
+    BOT_TOKEN = os.environ.get("BOT_TOKEN", "7078174063:AAHh7wcXOvYEaLrEzW_Rc1X0lfarFIvXdMQ") # ⚠️ Required
     FORCE_SUB = os.environ.get('FORCE_SUB', '@animetamil_xyz') # ⚠️ Required
     AUTH_CHANNEL = int(FORCE_SUB) if FORCE_SUB and id_pattern.search(
     FORCE_SUB) else None
